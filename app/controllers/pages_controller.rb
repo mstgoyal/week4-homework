c03dd9e7ccd02e3c23fd676746c02238577361ca
Tina Goyal <tgoyal2014@kellogg.northwestern.edu>
2014-02-04 19:39:16 -0600

    Created Weather and New Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
   class PagesController < ApplicationController
+    def index
+      @name_list = ["Andrew", "Gary", "Anish"]
+    end
+
+    def show
+      @name = "Andrew"
+    end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Week4hw::Application.routes.draw do
 
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
+
+
+
   get "/" => "pages#home"
+  get '/index' => "pages#index"
+  get '/show' => "pages#show"
 
   # HINT: Add more routes here...
 
